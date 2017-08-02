ceaeeeca620d35e8694b4fc48e8ea8633077165c
Venkatesh Reddy Duvvuru@Office <mail4dvenkatesh@gmail.com>
2017-08-02 20:12:39 +0530

    Tried to add reconnect

--- OutboundService/OutboundService/DynamicsCRM.cs
+++ OutboundService/OutboundService/DynamicsCRM.cs
@@ -3,35 +3,47 @@ using Microsoft.Xrm.Sdk.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace OutboundService
 {
     class DynamicsCRM
     {
         public IOrganizationService _service;
+        public static int retry = 0;
 
-        internal IOrganizationService ConnectToMSCRM(string UserName, string Password, string SoapOrgServiceUri)
+        internal void ReConnectToMSCRM()
         {
-            try
+            if(retry < int.Parse(System.Configuration.ConfigurationManager.AppSettings["RetryCount"]))
             {
+                retry++;
+                ConnectToMSCRM();
+            }
+        }
+
+        internal IOrganizationService ConnectToMSCRM()
+        {
+            //try
+            //{
                 ClientCredentials credentials = new ClientCredentials();
-                credentials.UserName.UserName = UserName;
-                credentials.UserName.Password = Password;
-                Uri serviceUri = new Uri(SoapOrgServiceUri);
+                credentials.UserName.UserName = ConfigurationManager.AppSettings["UserName"];
+                credentials.UserName.Password = ConfigurationManager.AppSettings["Password"];
+                Uri serviceUri = new Uri(ConfigurationManager.AppSettings["SoapOrgServiceUri"]);
                 OrganizationServiceProxy proxy = new OrganizationServiceProxy(serviceUri, null, credentials, null);
                 proxy.EnableProxyTypes();
                 _service = (IOrganizationService)proxy;
                 return _service;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error while connecting to CRM " + ex.Message);
-                Console.ReadKey();
-                return null;
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error while connecting to CRM " + ex.Message);
+            //    Console.ReadKey();
+            //    return null;
+            //}
         }
     }
 }
--- OutboundService/OutboundService/OutboundServiceSvc.cs
+++ OutboundService/OutboundService/OutboundServiceSvc.cs
@@ -28,33 +28,59 @@ namespace OutboundService
 
         protected override void OnStart(string[] args)
         {
+            Functionality();
+        }
+
+        private static void Functionality()
+        {
             Common common = new Common();
             DynamicsCRM crm = new DynamicsCRM();
-            crm._service = crm.ConnectToMSCRM(@"dfcni\swathiy", "Pass@123", "http://192.168.87.5:5555/spslive31b/xrmservices/2011/organization.svc");
 
-            Guid userID = ((WhoAmIResponse)crm._service.Execute(new WhoAmIRequest())).UserId;
-
-            if (userID != null)
+            try
             {
-                //System.IO.File.Create(Environment.CurrentDirectory + "//" + DateTime.Today.ToShortDateString() + "//" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".txt");
-                //System.IO.FileStream fileName = System.IO.File.Create(common.GetDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt");
+                crm._service = crm.ConnectToMSCRM();
 
-                common.SetLogFile("Outbound");
+                Guid userID = ((WhoAmIResponse)crm._service.Execute(new WhoAmIRequest())).UserId;
 
-                QueryExpression query = new QueryExpression();
-                query.EntityName = "dfc_accountni";
-                query.ColumnSet = new ColumnSet("dfc_name");
-                query.TopCount = 10;
-                query.LinkEntities.Add(new LinkEntity("dfc_accountni", "dfc_accountnirecord", "dfc_accountnirecord", "dfc_accountnirecord", JoinOperator.Inner));
-                //query.Criteria.AddCondition("ticketnumber", ConditionOperator.Equal, "CAS-01548-Z5Z5F0");
-                EntityCollection results = crm._service.RetrieveMultiple(query);
-
-                foreach(Entity accountNI in results.Entities)
+                if (userID != null)
                 {
-                    common.Log(common.logFile, accountNI.GetAttributeValue<string>("dfc_name"));
-                }
+                    //System.IO.File.Create(Environment.CurrentDirectory + "//" + DateTime.Today.ToShortDateString() + "//" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".txt");
+                    //System.IO.FileStream fileName = System.IO.File.Create(common.GetDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt");
 
-                common.Log(common.logFile, "completd ya");
+                    common.SetLogFile("Outbound");
+
+                    QueryExpression query = new QueryExpression();
+                    query.EntityName = "dfc_accountnirecord";
+                    query.ColumnSet = new ColumnSet("dfc_name");
+                    query.TopCount = 10;
+                    query.LinkEntities.Add(new LinkEntity("dfc_accountni", "dfc_accountnirecord", "dfc_accountni", "dfc_accountni", JoinOperator.Inner));
+                    //query.Criteria.AddCondition("ticketnumber", ConditionOperator.Equal, "CAS-01548-Z5Z5F0");
+                    EntityCollection results = crm._service.RetrieveMultiple(query);
+
+                    foreach (Entity accountNI in results.Entities)
+                    {
+                        common.Log(common.logFile, accountNI.GetAttributeValue<string>("dfc_name"));
+                    }
+
+                    common.Log(common.logFile, "completd ya");
+                }
+            }
+            catch (System.Net.WebException ex)
+            {
+                crm.ReConnectToMSCRM();
+                Functionality();
+            }
+            catch (System.ServiceModel.CommunicationException ex)
+            {
+                crm.ReConnectToMSCRM();
+                Functionality();
+            }
+            catch (Exception ex)
+            {
+                common.Log(common.logFile, ex.ToString());
+            }
+            finally
+            {
             }
         }
 
