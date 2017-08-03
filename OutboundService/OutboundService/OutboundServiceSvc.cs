39505ff81ff1290586cc18cbfb1afbd2ff70e12b
Venkatesh Reddy Duvvuru@Office <mail4dvenkatesh@gmail.com>
2017-08-03 23:22:50 +0530

    Trying to add multiple link entities., but unable to add filter criteria on second link entity

--- OutboundService/OutboundService/OutboundServiceSvc.cs
+++ OutboundService/OutboundService/OutboundServiceSvc.cs
@@ -44,25 +44,97 @@ namespace OutboundService
 
                 if (userID != null)
                 {
-                    //System.IO.File.Create(Environment.CurrentDirectory + "//" + DateTime.Today.ToShortDateString() + "//" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".txt");
-                    //System.IO.FileStream fileName = System.IO.File.Create(common.GetDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt");
-
                     common.SetLogFile("Outbound");
 
                     QueryExpression query = new QueryExpression();
-                    query.EntityName = "dfc_accountnirecord";
-                    query.ColumnSet = new ColumnSet("dfc_name");
+                    query.EntityName = "wrmm_payment";
+                    query.ColumnSet = new ColumnSet(true);
                     query.TopCount = 10;
-                    query.LinkEntities.Add(new LinkEntity("dfc_accountni", "dfc_accountnirecord", "dfc_accountni", "dfc_accountni", JoinOperator.Inner));
-                    //query.Criteria.AddCondition("ticketnumber", ConditionOperator.Equal, "CAS-01548-Z5Z5F0");
+
+                    #region WorkingCode
+                    //query.LinkEntities.Add(new LinkEntity("wrmm_payment", "dfc_accountnirecord", "dfc_accountnirecord", "dfc_accountnirecordid", JoinOperator.Inner));
+                    //query.LinkEntities[0].EntityAlias = "temp1";
+                    //query.Criteria.AddCondition("dfc_accountnirecord", "dfc_invoicenumber", ConditionOperator.Equal, "WSP0005378");
+                    #endregion
+
+                    //query.AddLink("dfc_accountnirecord", "dfc_accountnirecord", "dfc_accountnirecordid", JoinOperator.Inner);
+                    //query.LinkEntities[0].EntityAlias = "temp1";
+                    //query.Criteria.AddCondition("temp1", "dfc_invoicenumber", ConditionOperator.Equal, "WSP0005378");
+                    //query.LinkEntities.Add(new LinkEntity("dfc_accountnirecord", "dfc_accountni", "dfc_accountni", "dfc_accountniid", JoinOperator.Inner));
+                    //query.LinkEntities[1].EntityAlias = "temp2";
+                    //query.Criteria.AddCondition("temp2", "dfc_isexported", ConditionOperator.Equal, 0);
+                    //query.Criteria.AddCondition("temp2", "dfc_name", ConditionOperator.Contains, "ap_supplier_inv_dfc_20022017102512.csv");
+
+
+                    //LinkEntity le1 = new LinkEntity("wrmm_payment", "dfc_accountnirecord", "dfc_accountnirecord", "dfc_accountnirecordid", JoinOperator.Inner);
+                    //le1.Columns = new ColumnSet("dfc_invoicenumber");
+                    //LinkEntity le2 = new LinkEntity("dfc_accountnirecord", "dfc_accountni", "dfc_accountni", "dfc_accountniid", JoinOperator.Inner);
+                    //le2.Columns = new ColumnSet("dfc_name", "dfc_isexported");
+
+                    //le1.LinkEntities.Add(le2);                
+
+                    //query.LinkEntities.Add(le1);
+
+
+
+
+                    query.AddLink("dfc_accountnirecord", "dfc_accountnirecord", "dfc_accountnirecordid", JoinOperator.Inner);
+                    query.LinkEntities[0].AddLink("dfc_accountni", "dfc_accountni", "dfc_accountniid", JoinOperator.Inner);
+
+                    FilterExpression fe = new FilterExpression(LogicalOperator.And);
+
+                    FilterExpression fe1 = new FilterExpression(LogicalOperator.And);
+                    FilterExpression fe2 = new FilterExpression(LogicalOperator.And);
+
+
+                    fe1.Conditions.Add(new ConditionExpression("dfc_isexported", ConditionOperator.Equal, false));
+                    fe2.Conditions.Add(new ConditionExpression("dfc_name", ConditionOperator.Contains, "ap_supplier_inv_dfc_20022017102512"));
+
+                    fe.AddFilter(fe1);
+                    fe.AddFilter(fe2);
+
+                    
+
+
+                    query.LinkEntities[0].EntityAlias = "temp1";
+                    query.LinkEntities[0].LinkEntities[0].EntityAlias = "temp2";
+
+                    query.LinkEntities[0].LinkEntities[0].LinkCriteria = fe;
+
+                    //query.LinkEntities[0].LinkEntities[0].LinkCriteria.AddCondition("dfc_isexported", ConditionOperator.Equal, false);
+                    //query.LinkEntities[0].LinkEntities[0].LinkCriteria.AddCondition("dfc_name", ConditionOperator.Contains, "ap_supplier_inv_dfc_20022017102512");
+
+                    //query.Criteria.AddCondition(query.LinkEntities[0].LinkEntities[0].EntityAlias, "dfc_isexported", ConditionOperator.Equal, false);
+                    //query.Criteria.AddCondition(query.LinkEntities[0].LinkEntities[0].EntityAlias, "dfc_name", ConditionOperator.Contains, "ap_supplier_inv_dfc_20022017102512.csv");
+
+
+                    //query.Criteria.AddCondition()
+
+                    //  Query using ConditionExpression and FilterExpression
+                    //ConditionExpression condition1 = new ConditionExpression();
+                    //condition1.AttributeName = "dfc_invoicenumber";
+                    //condition1.Operator = ConditionOperator.Equal;
+                    //condition1.Values.Add("WSP0005378");
+
+                    //FilterExpression filter1 = new FilterExpression();
+                    //filter1.Conditions.Add(condition1);
+
+                    //query.Criteria.AddFilter(filter1);
+
+                    //dfc_isexported dfc_name wrmm_invoicenumber
+                    //query.LinkEntities[0].Columns.AddColumns("dfc_name", "dfc_isexported");
+
                     EntityCollection results = crm._service.RetrieveMultiple(query);
 
-                    foreach (Entity accountNI in results.Entities)
+                    foreach (Entity payment in results.Entities)
                     {
-                        common.Log(common.logFile, accountNI.GetAttributeValue<string>("dfc_name"));
+                        string value = payment.GetAttributeValue<EntityReference>("wrmm_supplementarypayment").Name + " - " + payment.GetAttributeValue<DateTime>("wrmm_paymentperiodfrom").ToString() + " - " +
+                            payment.GetAttributeValue<DateTime>("wrmm_paymentperiodto").ToString() + " - " + payment.GetAttributeValue<DateTime>("wrmm_scheduledissuedate").ToString();
+
+                        common.Log(common.logFile, value);
                     }
 
-                    common.Log(common.logFile, "completd ya");
+                    common.Log(common.logFile, "completd ya");                    
                 }
             }
             catch (System.Net.WebException ex)
